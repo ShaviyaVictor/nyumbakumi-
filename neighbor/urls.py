98ea284e8b7930acbcc2675bf1ff365fcc47e164
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-19 09:28:30 +0300

    Register the url paths to the main project

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='neigbor-home'),
     path('neighbor/', views.neighbor, name='neighbor-neighbor'),
     path('agency/', views.agency, name='neigbor-agency'),
-    path('business', views.business, name='neigbor-business'),
-    
+    path('business/', views.business, name='neigbor-business'),
+
 ]
