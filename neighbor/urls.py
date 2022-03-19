ef068b49407a91dd8cf5f800408c9cb2fb8eb866
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-19 09:16:54 +0300

    Create the app url paths

---
+++ neighbor/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+
+    path('', views.home, name='neigbor-home'),
+    path('neighbor/', views.neighbor, name='neighbor-neighbor'),
+    path('agency/', views.agency, name='neigbor-agency'),
+    path('business', views.business, name='neigbor-business'),
+    
+]
