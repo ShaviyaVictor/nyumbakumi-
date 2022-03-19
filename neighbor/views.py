6a20c213181d399901f380cda01240816f544a53
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-19 11:00:21 +0300

    Create a dummy db data for rendering

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,12 +1,53 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
+
+# Creating dummy data for context calling
+n_posts = [
+
+  {
+    'n_author': 'Victor Shaviya',
+    'n_name': 'Danstone',
+    'n_location': 'Imara daima',
+    # 'n_image': '',
+    'n_title': 'Internet upgrade',
+    'n_update': 'We are upgrading the internet.',
+    'n_date_posted': '19 March, 2022',
+  },
+  {
+    'n_author': 'Josphine Mbaisi',
+    'n_name': 'Boulevard',
+    'n_location': '14 Street',
+    # 'n_image': '',
+    'n_title': 'Utilities condition',
+    'n_update': 'Utility checks will be done today.',
+    'n_date_posted': '28 March, 2022',
+  },
+  {
+    'n_author': 'Norris Ambune',
+    'n_name': 'Tel Aviv',
+    'n_location': 'Taj Mall',
+    # 'n_image': '',
+    'n_title': 'Relocation',
+    'n_update': 'I am to relocate in a few days.',
+    'n_date_posted': '01 April, 2022',
+  },
+
+]
 
 
 
 # Create your views here.
 def home(request) :
 
-  return render(request, 'neighbor/home.html')
+  context = {
+      'n_posts' : n_posts
+    }
+
+    
+  return render(request, 'neighbor/home.html', context)
 
 
 
