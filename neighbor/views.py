e5e570120b0f4080e25943655e7c3dc7914dbcd3
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-19 09:09:32 +0300

    Create functions to handle our url paths and web pages

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
 
 # Create your views here.
+def home(request) :
+
+  return HttpResponse('<h1>Home Page</h1>')
+
+
+
+def neighbor(request) :
+
+  return HttpResponse('<h1>Neighbors Page</h1>')
+
+
+
+def agency(request) :
+
+  return HttpResponse('<h1>Agency Page</h1>')
+
+
+
+def business(request) :
+
+  return HttpResponse('<h1>Business Page</h1>')
