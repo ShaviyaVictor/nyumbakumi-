d59eb24f8f27e6a89ef9f5c0fd942fe86cc908a2
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-19 12:32:29 +0300

    Add the NavBar

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home, name='neigbor-home'),
+    path('', views.home, name='neighbor-home'),
     path('neighbor/', views.neighbor, name='neighbor-neighbor'),
-    path('agency/', views.agency, name='neigbor-agency'),
-    path('business/', views.business, name='neigbor-business'),
+    path('agency/', views.agency, name='neighbor-agency'),
+    path('business/', views.business, name='neighbor-business'),
 
 ]
