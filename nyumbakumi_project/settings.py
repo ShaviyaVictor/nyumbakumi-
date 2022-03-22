9b5562b5ebbbff59245a04d0fdd1b8b4d1a75b60
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-22 08:34:01 +0300

    Add the env file and add the secret key there and configure it

--- nyumbakumi_project/settings.py
+++ nyumbakumi_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils.command.config import config
 from pathlib import Path
 import os
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a6q(imah=@yn(m&%i*%5bj2jr0&ybu-2^xk)zy%qbaz52jy_t%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
